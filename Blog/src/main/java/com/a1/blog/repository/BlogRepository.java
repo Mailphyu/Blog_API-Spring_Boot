59417001da74bfaec444bb636c6c1cc4534e39bb
Mailphyu <sanij.shrestha23@cps.edu.np>
2025-09-05 13:52:51 +0545

    Delete Blog directory

--- Blog/src/main/java/com/a1/blog/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.a1.blog.repository;
-
-import com.a1.blog.model.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-}
